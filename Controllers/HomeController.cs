d58b20624aa7bf1d206f2f80f5f8230b65e83d22
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2018-09-28 09:46:10 +0530

    Add files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using DatatableCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatatableCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+ 
+        public ActionResult GetEmployees()
+        {
+            using (MydataEntities dc = new MydataEntities())
+            {
+                var employees = dc.Employees.OrderBy(a => a.FirstName).ToList();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (MydataEntities dc = new MydataEntities())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Employee emp)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (MydataEntities dc = new MydataEntities())
+                {
+                    if (emp.EmployeeID > 0)
+                    {
+                        //Edit 
+                        var v = dc.Employees.Where(a => a.EmployeeID == emp.EmployeeID).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.FirstName = emp.FirstName;
+                            v.LastName = emp.LastName;
+                            v.EmailID = emp.EmailID;
+                            v.City = emp.City;
+                            v.Country = emp.Country;
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        dc.Employees.Add(emp);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (MydataEntities dc = new MydataEntities())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            bool status = false;
+            using (MydataEntities dc = new MydataEntities())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Employees.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
